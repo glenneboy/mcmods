b4b0c00cc58dc328e587eac789ee24e4235172b9
Glenn Dale <glenn.dale@nokia.com>
2013-06-14 12:32:15 -0400

    asdf
    asdf

--- mod_TestCustomPacket.java
+++ mod_TestCustomPacket.java
@@ -11,8 +11,8 @@ import net.minecraft.client.Minecraft;
 public class mod_TestCustomPacket extends BaseMod{
     
 
-	private static final File gmDirectory = new File(Minecraft.getMinecraftDir(), "/Minecade_GM/");
-	private static final File gmLog = new File(gmDirectory, "Minecade_250Packets.txt");
+	private static final File gmDirectory = new File(Minecraft.getMinecraftDir(), "/Packets_Log/");
+	private static final File gmLog = new File(gmDirectory, "Custom_250Packets.txt");
 
 	
     public mod_TestCustomPacket() 
@@ -33,51 +33,40 @@ public class mod_TestCustomPacket extends BaseMod{
     {
         ModLoader.setInGameHook(this, true, false);
         ModLoader.registerPacketChannel(this, "scb");
-        ModLoader.registerPacketChannel(this, "walls");
 
     }
-
 
     
     // ModLoader @ MC 1.3+
     public void clientCustomPayload(NetClientHandler clientHandler, Packet250CustomPayload packet250custompayload)
     {
     	this.storeAction("CLIENT Packet 250 received!!");
+    	System.out.println("CLIENT Packet 250 received!!");
 
-    }
-    
-    
-    public void serverCustomPayload(NetServerHandler clientHandler, Packet250CustomPayload packet250custompayload){
-			
-    	this.storeAction("SERVER Packet 250 received!!");
-    	
     }
     
 
     public static void sendMessage(String channel, String message, NetClientHandler netclienthandler) {
         Packet250CustomPayload plugin = new Packet250CustomPayload();
-        plugin.channel = channel;
+        plugin.channel = "REGISTER";
         plugin.data = message.getBytes();
         plugin.length = message.length();
 
-		netclienthandler.addToSendQueue(plugin);
+		netclienthandler.addToSendQueue(plugin);	
+		System.out.println("REGISTER MESSAGE SENT TO SERVER.... ");
     }
 
     
-    // ModLoader @ MC 1.2.5
-    public void serverConnect(NetClientHandler netclienthandler) {
-        sendMessage("scb", "REGISTER", netclienthandler);
-        sendMessage("walls", "REGISTER", netclienthandler);
-    }
 
     // ModLoader @ MC 1.3+
     public void clientConnect(NetClientHandler netclienthandler) {
-        serverConnect(netclienthandler);
+//        serverConnect(netclienthandler);
+    	
+    	this.sendMessage("scb", "SOME MESSAGE", netclienthandler);
     }
 
     
     /**
-     * store the action the GM took.....
      * @throws IOException
      */
     public void storeAction(String aAction){
